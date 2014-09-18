3919df17f1b3a0ac4ca1e3367d81af0e5f9111bc
Jarred Laver <jlavier@jsu.edu>
2014-09-17 21:28:43 -0500

    Working Game Without GUI
    Unit tests run smooth with assert statements, but no way to implement
    the acceptance tests without the GUI involved.

--- TicTacToe/src/main/java/game/TicTacToe.java
+++ TicTacToe/src/main/java/game/TicTacToe.java
@@ -1,6 +1,8 @@
 package game;
 
-public class TicTacToe {
+import java.util.Scanner;
+
+public final class TicTacToe {
 	public String[][] board;
 	public String currentPlayer;
 	public final int ROW = 3;
@@ -16,62 +18,129 @@ public class TicTacToe {
 	public void startGame() {
 	    for(int row = 0; row < 3; row++) {
 	        for(int col = 0; col < 3; col++) {
-	          board[row][col] = "";
+	          board[row][col] = " ";
 	        }
 	     }
 	}
 	
-	public String getLocation(int row, int col) {
-		return (board[row][col]);
-	}
+	public String checkLocation(int row, int col) {
 	
-	public void currentLocation(int row, int col) {
-		if (board[row][col] == "") {
-			board[row][col] = currentPlayer;
+		if(board[row][col]== "X"){
+			return "X";
+		}
+		else if (board[row][col] =="O"){
+			return "O";
+		}
+		else {
+			return " ";
 		}
 	}
-	 
-	public boolean checkBoard(String one, String two, String three) {
-		  	return ((one != "") && (one == two) && (two == three));
+	
+	public boolean checkBoard(String one, String two, String three) { 
+	
+		 return((one != " ") && (one == two) && (two == three));
 	}
 	
-	public boolean winDiagonally() {
-		 return ((checkBoard(board[0][0], board[1][1], 
-		 board[2][2]) == true) || (checkBoard(board[0][2], 
-		 board[1][1], board[2][0]) == true));
-	}
-	
-	public boolean winHorizontally() {
-		for (int row = 0; row < 3; row++) {
-			if (checkBoard(board[row][0], board[row][1], 
-			board[row][2]) == true) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean winner() {
-		return (winHorizontally() || winDiagonally());	    
-	}
+	public void markLocation(int row, int col) {
+
+    if (" ".equals(board[row][col])) {
+        board[row][col] = currentPlayer;           
+       } 
+    }
 
     public String getWinner() {
-		if (winner() == true) 
+	
+		if (checkRows() || checkColumns() || checkDiagonals()
+			== true) 
 		{
-			changePlayer();
 			return currentPlayer;
 		}
 		else {
-			return "TIE";
+			return "TIE"; 
 		}
     }
 	
 	public void changePlayer() {
-    	if (currentPlayer == "X") {
+    	if ("X".equals(currentPlayer)) {
     		currentPlayer = "O";
     	}
     	else {
         	currentPlayer = "X";
     	}
+    }
+	
+    public void printBoard() {
+        System.out.println("-------------");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+	
+
+	// These last 3 methods are good to go!
+    public boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (checkBoard(board[i][0], board[i][1], board[i][1]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (checkBoard(board[0][i], board[1][i], board[2][i]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDiagonals() {
+        if ((checkBoard(board[0][0], board[1][1], board[2][2])) || 
+		(checkBoard(board[0][2], board[1][1], board[2][0]))){
+			return true;
+		}
+		else{
+			return false;
+		}
+    }
+
+    
+    // Game works great in command prompt 
+    // but does not have any GUI 
+    
+    public static void main (String[] args){
+		TicTacToe game = new TicTacToe();
+        Scanner first = new Scanner(System.in);
+        Scanner second = new Scanner(System.in);
+		
+        for(int i = 0;i < 8;i++){
+             System.out.println("Player " + 
+                     game.currentPlayer + " mark your location");
+					 
+             int row = first.nextInt();
+             int col = second.nextInt();
+             game.markLocation(row,col);
+             game.printBoard(); 
+             
+             if (game.checkColumns() || game.checkRows() 
+                     || game.checkDiagonals() == true){
+                 break;
+             }
+             game.changePlayer();
+        }
+		if(game.getWinner().equals("X") || game.getWinner().equals("O")){
+            System.out.println("Player " + game.getWinner()+ " won!");
+        }
+        else{
+            System.out.println("It's a " + game.getWinner());
+        }
     }
 }
--- TicTacToe/src/test/java/game/TicTacToeTest.java
+++ TicTacToe/src/test/java/game/TicTacToeTest.java
@@ -5,28 +5,89 @@ import static org.junit.Assert.*;
 
 public class TicTacToeTest {
 	
-	TicTacToe game = new TicTacToe();
-	
     @Test
-    public void testWinDiagonallyAsX() {
-		game.currentLocation(1,1);
-		game.getLocation(1,1);
-		game.getWinner();
+    public void winDiagonallyAsX() {
+		TicTacToe game = new TicTacToe(); 
+		game.markLocation(1,1);
+		// check if "X" is being marked
+		assertEquals("X",game.checkLocation(1,1)); 
+		game.changePlayer();
+		game.markLocation(0,1);
+		// check if "O" is being marked
+		assertEquals("O",game.checkLocation(0,1));  
+		game.changePlayer();
+		game.markLocation(0,0);
+		game.checkLocation(0,0);
+		game.changePlayer();
+		game.markLocation(0,2);
+		game.checkLocation(0,2);
+		game.changePlayer();
+		game.markLocation(2,2);
+		game.checkLocation(2,2);
+		assertTrue(game.checkDiagonals()); // check if "X" has won diagonally
+		assertEquals("X",game.getWinner()); // "X" wins
     }
     
     @Test
-    public void testWinHorizontallyAsO() {
-		game.currentLocation(1,1);
-		game.getLocation(1,1);
+    public void winHorizontallyAsO() {
+		TicTacToe game = new TicTacToe(); 
+		game.markLocation(1,1);
+		assertEquals("X",game.checkLocation(1,1)); 
 		game.changePlayer();
-		game.currentLocation(2,0);
-		game.getLocation(2,0);
+		game.markLocation(2,0);
+		assertEquals("O",game.checkLocation(2,0)); 
 		game.changePlayer();
-		game.getWinner();
+		game.markLocation(0,0);
+		game.checkLocation(0,0);
+		game.changePlayer();
+		game.markLocation(2,2);
+		game.checkLocation(2,2);
+		game.changePlayer();
+		game.markLocation(0,2);
+		game.checkLocation(0,2);
+		game.changePlayer();
+		game.markLocation(2,1);
+		game.checkLocation(2,1);
+		assertTrue(game.checkRows()); // check if "O" has won horizontally
+		assertEquals("O",game.getWinner()); // "O" wins
+
     }
 	
 	@Test
-	public void testForceATie() {
-		assertEquals("TIE",game.getWinner());
+	public void forceATie() {
+		TicTacToe game = new TicTacToe(); 
+		game.markLocation(1,1);
+		assertEquals("X",game.checkLocation(1,1)); 
+		game.changePlayer();
+		game.markLocation(0,0);
+		assertEquals("O",game.checkLocation(0,0)); 
+		game.changePlayer();
+		game.markLocation(2,0);
+		game.checkLocation(2,0);
+		game.changePlayer();
+		game.markLocation(0,2);
+		game.checkLocation(0,2);
+		game.changePlayer();
+		game.markLocation(0,1);
+		game.checkLocation(0,1);
+		game.changePlayer();
+		game.markLocation(2,1);
+		game.checkLocation(2,1);
+		game.changePlayer();
+		game.markLocation(1,2);
+		game.checkLocation(1,2);
+		game.changePlayer();
+		game.markLocation(1,0);
+		game.checkLocation(1,0);
+		game.changePlayer();
+		game.markLocation(2,2);
+		game.checkLocation(2,2);
+		
+		// Checks to see if their is no winner 
+		assertFalse(game.checkRows()); 
+		assertFalse(game.checkColumns());
+		assertFalse(game.checkDiagonals());
+		assertEquals("TIE",game.getWinner()); // It's a "TIE"
+
 	}
 }
